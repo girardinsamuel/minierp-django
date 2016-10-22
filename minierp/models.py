670371411e74e8d377d0ba96a428e0a2da702a5c
sgirardin <samuelgirardin@gmail.com>
2016-10-22 18:41:14 +0200

    add app

---
+++ minierp/models.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+
